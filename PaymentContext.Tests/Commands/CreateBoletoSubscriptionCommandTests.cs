9a3c6e027bbbe57a76f24d4f5ccb7d9c721608ca
Christian Silva <christian.eds@hotmail.com>
2019-06-28 03:11:42 -0300

    Add project files.

---
+++ PaymentContext.Tests/Commands/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests.Commands
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorWhenNameIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand
+            {
+                FirstName = ""
+            };
+
+            command.Validate();
+            Assert.AreEqual(false, command.Valid);
+        }
+    }
+}
